ee9e6bb61a18d637a24b60e842b93e8b1e89584a
kennysFE <metmoi556@gmail.com>
2024-11-13 21:26:05 +0700

    first api

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,31 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/create")
+    public String createNewUser() {
+        User user = new User();
+        user.setName("Nguyen Minh Hoang");
+        user.setEmail("hoangpikac@gmail.com");
+        user.setPassword("123456");
+
+        this.userService.handleCreateUser(user);
+
+        return " create user ";
+
+    }
+
+}
