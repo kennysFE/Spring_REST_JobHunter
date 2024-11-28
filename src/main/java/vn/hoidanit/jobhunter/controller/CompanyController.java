3d55aaa336e54927e0a638872037a1d240394112
kennysFE <metmoi556@gmail.com>
2024-11-28 22:58:52 +0700

    CRUD model company

--- src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.Company;
 import vn.hoidanit.jobhunter.service.CompanyService;
@@ -10,6 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class CompanyController {
@@ -24,6 +30,37 @@ public class CompanyController {
     public ResponseEntity<?> createCompany(@Valid @RequestBody Company reqCompany) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(this.companyService.handleCreateCompany(reqCompany));
+
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<List<Company>> getAllCompany() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.companyService.fetchAllCompanies());
+
+    }
+
+    @GetMapping("/companies/{companyId}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable("companyId") long companyId) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.companyService.fetchCompanyById(companyId));
+
+    }
+
+    @PutMapping("companies")
+    public ResponseEntity<Company> updateCompany(@RequestBody Company reqCompany) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.companyService.handleUpdateCompany(reqCompany));
+
+    }
+
+    @DeleteMapping("/companies/{companyId}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable("companyId") long companyId) {
+
+        this.companyService.handleDeleteCompany(companyId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
@@ -16,6 +19,36 @@ public class CompanyService {
 
     public Company handleCreateCompany(Company company) {
         return this.companyRepository.save(company);
+    }
+
+    // Get all company without pagination
+    public List<Company> fetchAllCompanies() {
+        return this.companyRepository.findAll();
+    }
+
+    public Company fetchCompanyById(long companyId) {
+        Optional<Company> currentCompany = this.companyRepository.findById(companyId);
+        if (currentCompany.isPresent()) {
+            return currentCompany.get();
+        }
+        return null;
+    }
+
+    public Company handleUpdateCompany(Company reqCompany) {
+        Company currentCompany = this.fetchCompanyById(reqCompany.getId());
+        if (currentCompany != null) {
+            currentCompany.setName(reqCompany.getName());
+            currentCompany.setDescription(reqCompany.getDescription());
+            currentCompany.setAddress(reqCompany.getAddress());
+            currentCompany.setLogo(reqCompany.getLogo());
+
+            return this.companyRepository.save(currentCompany);
+        }
+        return currentCompany;
+    }
+
+    public void handleDeleteCompany(long companyId) {
+        this.companyRepository.deleteById(companyId);
     }
 
 }
