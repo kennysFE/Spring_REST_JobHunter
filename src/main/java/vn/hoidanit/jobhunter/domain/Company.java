7b3d383eec19372b9c5a4ec9ab46a29079ce8f6e
kennysFE <metmoi556@gmail.com>
2024-12-09 14:43:11 +0700

    Model relationship (User, Company)

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -1,14 +1,15 @@
 package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -36,8 +37,6 @@ public class Company {
 
     private String logo;
 
-    // format timestamp
-    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
 
     private Instant updatedAt;
@@ -45,6 +44,9 @@ public class Company {
     private String createdBy;
 
     private String updatedBy;
+
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    List<User> users;
 
     @PrePersist
     public void handleCreateAt() {
