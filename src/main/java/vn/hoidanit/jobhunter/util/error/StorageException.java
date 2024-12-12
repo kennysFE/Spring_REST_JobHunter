35cc9f3b06b65b32b16e8e816c1cd259e5244152
kennysFE <metmoi556@gmail.com>
2024-12-12 17:55:33 +0700

    Complete upload file module

---
+++ src/main/java/vn/hoidanit/jobhunter/util/error/StorageException.java
@@ -0,0 +1,9 @@
+package vn.hoidanit.jobhunter.util.error;
+
+public class StorageException extends Exception {
+
+    // Contructor exception message
+    public StorageException(String message) {
+        super(message);
+    }
+}
