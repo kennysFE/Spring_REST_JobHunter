500528bf5eb5adbab53579fac4f4cce0195ee787
kennysFE <metmoi556@gmail.com>
2024-12-13 15:13:07 +0700

    Complete CRUD model resumes

--- src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
@@ -12,6 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
@@ -26,8 +27,10 @@ public class Resume {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "email không được để trống")
     private String email;
 
+    @NotBlank(message = "url không được để trống (upload cv chưa thành công)")
     private String url;
 
     @Enumerated(EnumType.STRING)
